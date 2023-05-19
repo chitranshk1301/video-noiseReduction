796d86240c43de2060df0dd04ae83f7a2dc07fb4
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-05-20 00:54:38 +0530

    reducer working (hard coded)

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+// TODO: 
+// 1. delete files after they are done processing (both input and output)
+// 2. google drive link (that'll require database we'll use mongo) 
+// 3. Queing service 
+
+// 
+
 const express = require('express');
 const fs = require('fs');
 const app = express();
 const multer = require('multer');
 const morgan = require('morgan');
-const { time } = require('console');
+const exec = require('child_process').exec;
+import { path as ffmpeg } from '@ffmpeg-installer/ffmpeg';
 
 app.use(morgan('dev'));
 
@@ -22,9 +30,24 @@ const storage = multer.diskStorage({
         cb(null, './public/uploads/');
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname);
+        cb(null, 'my-video.mov');
     }
 });
+
+const inputPath = './public/uploads/my-video.mov';
+const outputPath = './public/uploads/output-video.mov';
+
+// noise reducer
+const noiseReducer = (req, res) => {
+    exec(`${ffmpeg} -i ${inputPath} -af "highpass=f=200,lowpass=f=3000,afftdn=nf=-25" ${outputPath}`, (err, stdout, stderr) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    })
+}
 
 // endpoints
 app.get('/',(req,res) => {
@@ -63,5 +86,14 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     }
 })
 
+app.get('/run', (req, res) => {
+    noiseReducer();
+    res.send('Noise reducer is running');
+
+})
+
 app.listen(3000, () => console.log('Server started on port 3000'));
 
+
+
+ 
